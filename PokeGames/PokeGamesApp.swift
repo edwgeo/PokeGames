885e449207b63480b195a3b01d2d1bc4ce91db04
Edwin George <edwgeo@gmail.com>
2024-02-21 23:12:11 -0800

    Initial Commit

---
+++ PokeGames/PokeGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeGamesApp.swift
+//  PokeGames
+//
+//  Created by Edwin George on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
